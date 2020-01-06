cafff1bfdb8f0c1ce65a876f9f91dae1ce192c2c
Pysched <murphyjdarren@gmail.com>
2020-01-06 20:15:50 +0000

    udated java

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,47 +1,51 @@
 function userInformationHTML(user) {
-    return`
-    <h2>${user.name}
-    <span class="small-name">
-    (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
-    </span>
-    </h2>
-    <div class="gh-content">
-        <div class="gh-avatar">
-        <a href="${user.html_url}" target="_blank"
-            <imh src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
-        </a>
-        </div>
-        <p> Followers: ${user.followers} - Following ${user.followers}<br> Repos:${user.public_repos}</p>
-    </div>`;
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
 }
 
-function fetchGitHubInformation(event){
+function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
-    if(!username){
+    if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
+
     $("#gh-user-data").html(
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
-    </div>  `  );
+        </div>`);
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response){
-            var userDAta=response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
-            }, function (errorResponse) {
-                if(errorResponse.status === 404) {
-                    $("#gh-user-data").html(`<h2>No infor found for users ${username}</h2>`);
-                }else {
-                    console.log(errorResponse);
-                    $("#gh-user-data").html(
-                       `<h2>ERror: ${errorResponse.responseJSON.message}</h2>`
-                    )
-                }
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
-    )
+        });
 }
